66dc47b93ff36f30124475f6626e8659fcb53998
Ndiema-S <you@example.com>
2022-07-18 00:49:45 +0300

    main

--- index.js
+++ index.js
@@ -53,3 +53,53 @@ const totalProduction = () => {
 
 
 }
+
+
+const incomeOverTime = () => {
+    sellling_price = document.getElementById("sellling_price").value;
+    event.preventDefault();
+    let TotalMilk = 0;
+
+        ShadeA = milkProductions[0]["ShadeA"]
+        ShadeASum = 0;
+        for(shade_a = 0; shade_a < ShadeA.length; shade_a++){
+            ShadeASum += ShadeA[shade_a]
+
+        }
+
+        ShadeB = milkProductions[1]["ShadeB"];
+        ShadeBSum = 0;
+        for(shade_b= 0; shade_b < ShadeB.length; shade_b++){
+            ShadeBSum += ShadeB[shade_b];
+
+        }
+
+        
+        ShadeC = milkProductions[2]["ShadeC"];
+        ShadeCSum = 0;
+        for(shade_c= 0; shade_c< ShadeC.length; shade_c++){
+            ShadeCSum += ShadeC[shade_c];
+
+        }
+
+        ShadeD = milkProductions[3]["ShadeD"];
+        ShadeDSum = 0;
+        for(shade_d= 0; shade_d< ShadeD.length; shade_d++){
+            ShadeDSum += ShadeD[shade_d];
+
+        }        
+
+        TotalMilk = ShadeASum + ShadeBSum + ShadeCSum + ShadeDSum
+
+
+    let totalMilkProduction =  TotalMilk * sellling_price
+    document.getElementById("Sum").innerHTML = "Your weekly income will be ksh " + totalMilkProduction;
+    // return totalMilkProduction;
+}
+
+
+
+
+
+
+
