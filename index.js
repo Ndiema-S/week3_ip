e405e14040cbfb14b8ce908e1197f5e8ffdeb344
Ndiema-S <you@example.com>
2022-07-18 00:47:46 +0300

    add variables

--- index.js
+++ index.js
@@ -7,3 +7,49 @@ let milkProductions = [
 {"ShadeD" :  [510, 540,  549, 234,  567,  678, 543]}
 
 ];
+
+const totalProduction = () => {
+    let TotalMilk = 0;
+
+        ShadeA = milkProductions[0]["ShadeA"]
+        ShadeASum = 0;
+        for(shade_a = 0; shade_a < ShadeA.length; shade_a++){
+            ShadeASum += ShadeA[shade_a]
+
+        }
+
+        ShadeB = milkProductions[1]["ShadeB"];
+        ShadeBSum = 0;
+        for(shade_b= 0; shade_b < ShadeB.length; shade_b++){
+            ShadeBSum += ShadeB[shade_b];
+
+        }
+
+        
+        ShadeC = milkProductions[2]["ShadeC"];
+        ShadeCSum = 0;
+        for(shade_c= 0; shade_c< ShadeC.length; shade_c++){
+            ShadeCSum += ShadeC[shade_c];
+
+        }
+
+        ShadeD = milkProductions[3]["ShadeD"];
+        ShadeDSum = 0;
+        for(shade_d= 0; shade_d< ShadeD.length; shade_d++){
+            ShadeDSum += ShadeD[shade_d];
+
+        }        
+
+        TotalMilk = ShadeASum + ShadeBSum + ShadeCSum + ShadeDSum
+        let ShadeAProduction = "Your prodution in Shade A is " + ShadeASum + " litres per week."
+        let ShadeBProduction = "Your prodution in Shade B is " + ShadeBSum + " litres per week."
+        let ShadeCProduction = "Your prodution in Shade C is " + ShadeCSum + " litres per week."
+        let ShadeDProduction = "Your prodution in Shade D is " + ShadeDSum + " litres per week."
+        let TotaMilkAmount = "Total amount of mil produced by all shades is " + TotalMilk + " litres per week."
+
+        
+        document.getElementById("total-production").innerHTML = "<p>"+ ShadeAProduction +"<p>" + "<p>"+ 
+            ShadeBProduction +"<p>" + "<p>"+ ShadeCProduction +"<p>"  + "<p>"+ ShadeDProduction +"<p>"  + "<p>"+ TotaMilkAmount +"<p>"
+
+
+}
