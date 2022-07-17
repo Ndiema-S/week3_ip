d71b22eb2366e81dbded96004b34f2c1b050a6b5
Ndiema-S <you@example.com>
2022-07-18 00:43:59 +0300

    main

--- index.js
+++ index.js
@@ -1,60 +1,9 @@
+let milkProductions = [
+    {
+"ShadeA" :  [510, 456,  549, 569,  569,  553, 564]},
+{"ShadeB" :[510, 540,  549, 123,  569,  553, 234]}
+,
+{"ShadeC" :  [876, 234,  549, 678,  345,  553, 564]},
+{"ShadeD" :  [510, 540,  549, 234,  567,  678, 543]}
 
-
-function process() {
-    var name = String(document.form.InputName.value);
-    var a = Number(document.form.InputA.value);
-
-
-    var name = String(document.form.InputName.value);
-    var b = Number(document.form.InputB.value);
-
-    var name = String(document.form.InputName.value);
-    var c = Number(document.form.InputC.value);
-
-    var name = String(document.form.InputName.value);
-    var d = Number(document.form.InputD.value);
-
-    document.getElementById('output1').innerHTML = "your production in shed A " +
-        name + " " + a + "litres per day ";
-    document.getElementById('output2').innerHTML = "your production in shed B" +
-        name + " " + b + "litres per day ";
-    document.getElementById('output3').innerHTML = "your production in shed C" +
-        name + " " + c + "litres per day ";
-    document.getElementById('output4').innerHTML = "your production in shed D" +
-        name + " " + d + "litres per day ";
-
-    var x = a + b + c + d;
-    document.getElementById('output').innerHTML = "your total production in all sheds is " +
-        name + " " + x + "litres";
-}
-
-var a, b, c, d, x;
-
-a.forEach(function (entry) {
-    console.log(entry);
-});
-
-
-(function () {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var month_selected = (new Date).getMonth(); 
-    var option = '';
-    option = '<option>Month</option>'; 
-
-    for (let i = 0; i < months.length; i++) {
-        let month_number = (i + 1);
-
-        
-        let month = (month_number <= 9) ? '0' + month_number : month_number;
-        let selected = (i === month_selected ? ' selected' : '');
-        
-        option += '<option value="' + month + '"' + selected + '>' + months[i] + '</option>';
-    }
-document.getElementById("month").innerHTML = option;
-document.getElementById("month1").innerHTML = option;
-document.getElementById("month2").innerHTML = option;
-document.getElementById("month3").innerHTML = option;
-})();
-
-
-
+];
