9667cbf23140fd1f801cb7b782b3a9ba0ad42c68
Ndiema-S <you@example.com>
2022-07-17 17:20:19 +0300

    edit html

--- index.js
+++ index.js
@@ -34,32 +34,32 @@ a.forEach(function (entry) {
     console.log(entry);
 });
 
+(function () {
+       let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+       var month_selected = (new Date).getMonth(); // current month
+       var option = '';
+       option = '<option>Month</option>'; // first option
+
+       for (let i = 0; i < months.length; i++) {
+         let month_number = (i + 1);
+
+         // value month number with 0. [01 02 03 04..]
+         let month = (month_number <= 9) ? '0' + month_number : month_number;
+
+         // or value month number. [1 2 3 4..]
+         // let month = month_number;
+
+         // or value month names. [January February]
+         // let month = months[i];
+
+         let selected = (i === month_selected ? ' selected' : '');
+         option += '<option value="' + month + '"' + selected + '>' + months[i] + '</option>';
+       }
+       document.getElementById("month").innerHTML = option;
+       document.getElementById("month1").innerHTML = option;
+       document.getElementById("month2").innerHTML = option;
+       document.getElementById("month3").innerHTML = option;
+     })();
 
 
-(function ())
-{
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var month_selected = (new Date).getMonth();
-    var option = '';
-    option = '<option>Month</option>';
 
-    for (let i = 0; i < months.length; i++) {
-        let month_number = (i + 1);
-
-
-        let month = (month_number <= 9) ? '0' + month_number : month_number;
-
-
-
-        let selected = (i === month_selected ? ' selected' : '');
-        option += '<option value="' + month + '"' + selected + '>' + months[i] + '</option>';
-    }
-    {
-        document.getElementById("month").innerHTML = option;
-        document.getElementById("month1").innerHTML = option;
-        document.getElementById("month2").innerHTML = option;
-        document.getElementById("month3").innerHTML = option;
-    }
-}
-
-();
